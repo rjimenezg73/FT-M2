03d28496af87b55a669237d826950c8f3be0f3bc
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-08-09 16:34:57 -0500

    M2 Clase 06

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
+  
   return (
     <div>
-      <button onClick={props.onClose}>X</button>
+      <button onClick={onClose}>X</button>
+      <h3>{name}</h3>
+      <div>
+        <p>Min</p>
+        <p>{min}</p>
+        <p>Max</p>
+        <p>{max}</p>
+      </div>
+      <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="Imégen del Tiempo"></img>
     </div>
   )
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -2,5 +2,10 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div>
+      <input type='text' placeholder='Ciudad...'></input>
+      <button Onclick={() => props.OnSearch('Busca tu Ciudad...')}>Search</button>
+    </div>
+  )
 };
