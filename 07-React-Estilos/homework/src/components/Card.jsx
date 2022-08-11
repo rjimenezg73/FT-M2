311ede62ac3053e080bb655d35e2900dfd293263
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-08-11 13:21:21 -0500

    07 Estilos

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import styles from '../styles/Card.module.css';
 
-export default function Card(props) {
+export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  
+  return (
+    <div className={styles.card}>
+      <button onClick={onClose} className={`${styles.btn} ${styles.btnColor}`} >X</button>
+      
+      <h3>{name}</h3>
+      <div className={styles.middleDiv}>
+        <p>Min</p>
+        <p>{min}</p>
+        <p>Max</p>
+        <p>{max}</p>
+      </div>
+      <img className={styles.img} src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="Imégen del Tiempo"></img>
+    </div>
+  )
 };
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import styles from '../styles/SearchBar.module.css';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div className={styles.searchBar}>
+      <input type='text' placeholder='Ciudad...'></input>
+      <button Onclick={() => props.OnSearch('Busca tu Ciudad...')}>Search</button>
+    </div>
+  )
 };
